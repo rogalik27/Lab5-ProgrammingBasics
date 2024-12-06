474620cc8c2e1efb4ff8a89d2cd984b705d6f781
rogalik <rogalik.gh@gmail.com>
2024-12-06 14:32:09 +0100

    Optimised and fixed the first task in Tasks.cpp

--- Tasks.cpp
+++ Tasks.cpp
@@ -1,10 +1,10 @@
 ﻿#include "Tasks.h"
 #include <iostream>
 
-const int ARRAY_SIZE = 5;
-
+//const int ARRAY_SIZE = 5;
+using namespace std;
 double findMin(const double array[], int size) {
-    double minVal = std::numeric_limits<double>::max();
+    double minVal = 9999999;
     for (int i = 0; i < size; ++i) {
         if (array[i] < minVal) {
             minVal = array[i];
@@ -14,7 +14,7 @@ double findMin(const double array[], int size) {
 }
 
 double findMax(const double array[], int size) {
-    double maxVal = std::numeric_limits<double>::lowest();
+    double maxVal = -999999;
     for (int i = 0; i < size; ++i) {
         if (array[i] > maxVal) {
             maxVal = array[i];
@@ -24,10 +24,11 @@ double findMax(const double array[], int size) {
 }
 //function to call the task 1 in the main file 
 int Tasks::task1main() {
-    double array[ARRAY_SIZE] = { 1.5, -3.2, 7.8, 0.4, -1.1 };
-    double minVal = findMin(array, ARRAY_SIZE);
-    double maxVal = findMax(array, ARRAY_SIZE);
+    double array[5] = { 1.5, -3.2, 7.8, 0.4, -1.1 };
+    int elementAmount = sizeof(array) / 8;
+    double minVal = findMin(array, elementAmount);
+    double maxVal = findMax(array, elementAmount);
 
-    std::cout << "Sum of the   " << (minVal + maxVal) << std::endl;
+    cout << "Sum of the minimal and maximal value of the array is " << minVal << " + " << maxVal << " = " << minVal+maxVal<< endl;
     return 0;
 }
