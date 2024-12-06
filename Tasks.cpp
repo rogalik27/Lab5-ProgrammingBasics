74faa8ccdf6bbd6336020f0680246bd6591185cb
rogalik <rogalik.gh@gmail.com>
2024-12-06 14:55:17 +0100

    Commented and optimised some

--- Tasks.cpp
+++ Tasks.cpp
@@ -29,13 +29,13 @@ double findMax(const double array[], int size) {
 //function to call the task 1 in the main file 
 int Tasks::task1main() {
     //initialise an array and assign elements
-    double array[5] = {1.5, -3.2, 7.8, 0.4, -1.1 };
+    double array[5] = { 1.5, -3.2, 7.8, 0.4, -1.1 };
     //get the amount of elements in the array (in case user input is implemented and size isnt constant)
     int elementAmount = sizeof(array) / 8;
     //get the minimal values using previously defined functions
     double minVal = findMin(array, elementAmount);
     double maxVal = findMax(array, elementAmount);
     //display the values
-    cout << "Sum of the minimal and maximal value of the array is " << minVal << " + " << maxVal << " = " << minVal+maxVal<< endl;
+    cout << "Sum of the minimal and maximal value of the array is " << minVal << " + " << maxVal << " = " << minVal + maxVal << endl;
     return 0;
 }
